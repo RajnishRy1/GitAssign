6f8257679ea729b563e66379cc2124b37d1d8f88
Rajnish <virpaterajnish@gmail.com>
2020-09-07 17:25:17 +0530

    Adding Models/Employee

---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class Employees
+    {
+        public ArrayList Emp = new ArrayList();
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+}
